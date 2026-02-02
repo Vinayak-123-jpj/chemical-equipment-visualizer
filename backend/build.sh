5cb7373915f98fe33517f5556b3eea3c01563b96
Vinayak <vinayakdhyani27@gmail.com>
2026-02-02 17:03:59 +0530

    final render python fix

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install --upgrade pip
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
 python manage.py migrate
+python manage.py collectstatic --no-input
