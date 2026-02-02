b372516a2a5e9a65cb434c7f0b519909664c5429
Vinayak <vinayakdhyani27@gmail.com>
2026-02-03 00:35:21 +0530

    create demo user on deploy

--- backend/build.sh
+++ backend/build.sh
@@ -6,3 +6,12 @@ pip install -r requirements.txt
 
 python manage.py migrate
 python manage.py collectstatic --no-input
+
+# Create demo user
+echo "from django.contrib.auth.models import User; User.objects.get_or_create(username='demo', defaults={'email': 'demo@example.com'})[0].set_password('demo123'); User.objects.filter(username='demo').first().save()" | python manage.py shell
+
+
+
+
+
+
